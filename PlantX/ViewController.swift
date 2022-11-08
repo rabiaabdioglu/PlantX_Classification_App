189b4437c88eca4ebb775e86c6439d504a51bc69
Rabia Abdioğlu <rabiabdglu@gmail.com>
2022-11-08 21:59:15 +0300

    Proje_olusturma

---
+++ PlantX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantX
+//
+//  Created by MacbookAir on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
